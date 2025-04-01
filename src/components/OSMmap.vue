eba4ae5e35c99d43686ec41215c2b33bf5b5483c
ililyubov <ilylyubov@gmail.com>
2025-04-01 11:36:54 +0400

    change map layer

--- src/components/OSMmap.vue
+++ src/components/OSMmap.vue
@@ -2,6 +2,8 @@
   <div class="map-container">
     <div v-if="isLoading" id="loading-indicator">
       <img src="/filler.webp" alt="Loading..." />
+      <p>Please be patient, the loading might take a while.</p>
+
     </div>
     <div id="map"></div>
   </div>
@@ -20,39 +22,57 @@ export default defineComponent({
 
     // Fetch tracks from the API
     const fetchTracks = async () => {
-      try {
-        console.log('Preparing to fetch tracks...');
-        isLoading.value = true;
-        const response = await fetch('https://sakartrailo-backend.onrender.com/api/tracks/');
-        
-        if (!response.ok) {
-          throw new Error('Failed to fetch tracks');
-        }
+    const controller = new AbortController();
+    const timeoutId = setTimeout(() => controller.abort(), 60000); // 60 секунд
 
-        const data = await response.json();
-        console.log('Fetched data:', data); // Log the data received
-        tracks.value = data; // Store the fetched tracks
-      } catch (error) {
-        console.error('Error fetching tracks:', error); // Log any errors
-      } finally {
-        isLoading.value = false; // Set loading to false after fetch completes
+    try {
+      console.log('Preparing to fetch tracks...');
+      isLoading.value = true;
+
+      const response = await fetch('https://sakartrailo-backend.onrender.com/api/tracks/', {
+        signal: controller.signal, // Передаем сигнал аборта
+      });
+
+      if (!response.ok) {
+        throw new Error('Failed to fetch tracks');
       }
-    };
+
+      const data = await response.json();
+      console.log('Fetched data:', data);
+      tracks.value = data;
+    } catch (error) {
+      if (error instanceof Error) {
+      console.error('Error fetching tracks:', error.message);
+      } else {
+        console.error('Unknown error:', error);
+      }
+    } finally {
+      clearTimeout(timeoutId); // Очистка таймера
+      isLoading.value = false;
+    }
+  };
+
 
     onMounted(async () => {
       console.log('Component mounted, fetching tracks...');
       await fetchTracks(); // Fetch tracks on mount
+      
 
       const mapContainer = document.getElementById('map');
       if (!mapContainer) return;
 
-      const map = L.map(mapContainer).setView([41.6938, 44.8015], 13);
+      const map = L.map(mapContainer).setView([41.6938, 44.8015], 12); 
+      
+      // L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      //   maxZoom: 19,
+      //   attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+      // }).addTo(map);
 
-      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-      }).addTo(map);
-
+      L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/dark_matter/{z}/{x}/{y}{r}.png', {
+        attribution: '&copy; <a href="https://carto.com/">CARTO</a>',
+        subdomains: ['a', 'b', 'c'],
+        maxZoom: 20
+       }).addTo(map);
       // If there are tracks, plot them on the map
       if (tracks.value.length > 0) {
         console.log('Tracks available:', tracks.value); // Log the tracks
@@ -62,9 +82,9 @@ export default defineComponent({
             const trackPoints = track.nodes.map((node: any) => [node.lat, node.lon]);
 
             const labelIcons = {
-              yellow: '<img src="/icons/yellow_marker.svg" width="16">',
-              red: '<img src="/icons/red_marker.svg" width="16">',
-              blue: '<img src="/icons/blue_marker.svg" width="16">',
+              yellow: '<img src="/sakartrailo/yellow_marker.svg" width="16">',
+              red: '<img src="/sakartrailo/red_marker.svg" width="16">',
+              blue: '<img src="/sakartrailo/blue_marker.svg" width="16">',
             };
             const label = track.track_info.label?.toLowerCase() || ""; // Приводим к нижнему регистру для надежности
 
@@ -89,12 +109,14 @@ export default defineComponent({
             }).addTo(map);
 
             trackPolyline.bindPopup(`
-              <b>${track.track_info.name || "No Name"}</b><br>
+             <span style="font-weight: bold;">${track.track_info.name || "No Name"}</span><br>
               ${track.track_info.description ? `<i>${track.track_info.description}</i><br>` : ''}
               Distance: ${(track.track_info.distance ? track.track_info.distance.toFixed(2) : "N/A")} km<br>
+               ${icon ? `<span style="display: flex; align-items: center;">
+              <span>Marked by: </span>
               ${icon}
+            </span>` : ''}
             `);
-            map.fitBounds(trackPolyline.getBounds());
           } else {
             console.log('No nodes in this track:', track);
           }
@@ -131,10 +153,31 @@ export default defineComponent({
   justify-content: center;
   align-items: center;
   z-index: 1000; 
+  flex-direction: column;
+  align-items: center;
 }
 
 #loading-indicator img {
-  width: 500px;
-  height: 500px;
+  width: 300px;
+  height: 300px;
+}
+
+#loading-indicator p {
+  font-size: 30px;
+  color: #d0d0d0;
+  margin-top: 20px;
+  white-space: nowrap;
+}
+
+@media (max-width: 768px) {
+  #loading-indicator p {
+    font-size: 24px; 
+}
+}
+
+@media (max-width: 480px) {
+  #loading-indicator p {
+    font-size: 18px; 
+  }
 }
 </style>
